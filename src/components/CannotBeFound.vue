d593312769618cf44ff538a443fcba325892ebb2
ratsjosh <ratsjosh@outlook.com>
2017-12-27 16:09:45 +0800

    Changes to Vue router
    Add '404' page

---
+++ src/components/CannotBeFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="about" class="section is-large">
+    <div class="container">
+      <h1 class="title">404</h1>
+      <h2 class="subtitle">
+        Cannot be found :(
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'CannotBeFound',
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit SASS to this component only -->
+<style lang="sass" scoped>
+  .hero-body,
+  .section .container
+    .title
+      &subtitle
+    text-align: center
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import CannotBeFound from '@/components/CannotBeFound'
 
 Vue.use(Router)
 
 export default new Router({
+  base: '/Tacklebox',
   mode: 'history',
-  base: '/tacklebox',
   routes: [
     {
       path: '/',
-      alias: '/home',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '*',
+      name: 'CannotBeFound',
+      component: CannotBeFound,
     },
   ],
 })
